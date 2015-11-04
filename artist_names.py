57ff917ed3d90e0fcf92139d61ca222cf9bbaa41
George <georgewitteman@gmail.com>
2015-11-03 21:03:03 -0500

    Update artist_names.py

--- artist_names.py
+++ artist_names.py
@@ -4,17 +4,18 @@ import sys
 import re
 from bs4 import BeautifulSoup # To get everything
 
+# Remove the html tags
 def remove_tags(text):
     return re.sub('<[^>]*>', '', text)
 
+# Read the links file into string
 html = ""
 with open ("links.html", "r") as myfile:
     html=myfile.read().replace('\n', '')
 
-
-
 soup = BeautifulSoup(html, "html.parser")
-results = soup.findAll('a')
+results = soup.findAll('a') # Find all 'a' tags
 
+# For each result remove the html tags and print them out
 for result in results:
     print remove_tags(str(result)).lstrip()
